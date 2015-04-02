9ffd39a951dc6741ff643190c7eee09804db42d1
Sankar Narayanan <snkr.vln@gmail.com>
2015-04-02 13:07:30 +0530

    Check if this is what you were expecting.. Adding files...

---
+++ SampleTest/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  SampleTest
+//
+//  Created by Sankar Narayanan on 4/1/15.
+//  Copyright (c) 2015 Sankar Narayanan. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SampleTest/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  SampleTest
+//
+//  Created by Sankar Narayanan on 4/1/15.
+//  Copyright (c) 2015 Sankar Narayanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.registerNib(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+        tableView.registerNib(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 3
+    }
+    
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TableViewCell") as? TableViewCell
+        return cell!
+    }
+
+}
+
